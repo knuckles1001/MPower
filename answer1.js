48736ba1c5db280032df50187e34acade2421e69
knuckles1001 <knuckles1001@gmail.com>
2022-03-25 17:34:14 +0800

    Update answer1.js

--- answer1.js
+++ answer1.js
@@ -1,19 +1,41 @@
-function isSubset(arr1, arr2){
+//
 
-	for (i = 0; i < arr2.length; i++) {   
-	 	let in_arr=0;
-	 	for (j = 0; j < arr1.length; j++) {   
-	 		if(arr2[i]==arr1[j]){
-				in_arr=1;
-				break;
-			}
-		 }
-	 	if(in_arr==0){
-			document.getElementById("result").innerHTML = "Is not Subset";
-		 	return false;
-		 }
-	}
-	document.getElementById("result").innerHTML = "Is Subset";
-	return true;
+function isSubset(arr1, arr2){
 	
+	arr1=sort(arr1);
+	arr2=sort(arr2);
+	
+	let subSet=0;
+	for (i = 0; i < arr2.length; i++) {   
+		if(BigO(arr1, arr2[i])<0){
+			return false;
+		}
+	}
+	return true;
+}
+function BigO(array, key){
+    let min = 0 ;
+    let max = array.length -1;
+    while(min <= max){
+        let middle = Math.floor((min + max) /2);
+        if(array[middle] > key) {
+            max = middle -1;
+        }else if(array[middle] < key){
+            min = middle +1;
+        }else{
+            return middle;
+        }
+    }
+	return -1;
+}
+
+function sort(array){
+	for (i = 0; i < array.length; i++) {   
+		let chartext=array[i];
+		chartext=chartext.toUpperCase();
+		chartext=chartext.charCodeAt();
+		array[i]=chartext;
+	}
+	array.sort();
+	return array;
 }
