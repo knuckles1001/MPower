786672640eb6c7be16472b03358297bed9ec1845
knuckles1001 <knuckles1001@gmail.com>
2022-03-25 17:34:42 +0800

    Update answer3.java

--- answer3.java
+++ answer3.java
@@ -8,11 +8,12 @@
   if(n < 2 || n%1!=0){
 	 throw new Error ('Invalid input');
   } else{
-	    do {
+	  int looptime=n-2;
+	  int[] array = new int[looptime];
+	  for(int element:array){
   		  cur=cur + 1 / (n * (n -1));
 		    n=n - 1;
 	    }
-	    while (n !== 2);
 	  return 1 / n + cur;
   }
 }
