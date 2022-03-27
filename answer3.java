922e6418e87b88c67f9c12966f3eafd5a87a2b97
knuckles1001 <knuckles1001@gmail.com>
2022-03-27 15:34:32 +0800

    Update answer3.java

--- answer3.java
+++ answer3.java
@@ -5,9 +5,11 @@
    cur = 0;
   }
    
+ //  n must be bigger than 2 or is 2, the n must be integer so the remainder must be is 0 when it divided by 1
   if(n < 2 || n%1!=0){
 	 throw new Error ('Invalid input');
   } else{
+	  // if don't use while loop or do while, we use foreach to do the loop action. So we need create a new array to do this
 	  int looptime=n-2;
 	  int[] array = new int[looptime];
 	  for(int element:array){
